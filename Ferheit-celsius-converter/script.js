7782b94e5e3e275f45cfaddce69701c11dacb28c
TheoGits <theokrist07@gmail.com>
2024-01-16 20:35:22 +0100

    4.1
    Jeg føler at jeg begynner å få til javascript ganske bra.

---
+++ Ferheit-celsius-converter/script.js
@@ -0,0 +1,22 @@
+
+function farenheit_Til_Celsius(){
+    let farenheit = 100; //lokal label
+
+    let Celsius = (farenheit -32) / 1.8; // parentes for å regne ut farenheit -32 først
+
+    console.log(Celsius);
+
+}
+
+farenheit_Til_Celsius()
+
+function Celsius_Til_Farenheit(){
+    let Celsius = 37.77777777777778; //lokal label
+
+    let farenheit = Celsius * 1.8 +32; 
+
+    console.log(farenheit);
+
+}
+
+Celsius_Til_Farenheit()
