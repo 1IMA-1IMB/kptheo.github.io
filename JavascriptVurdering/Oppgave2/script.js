59ce4935d842bdca3150ed12f25e75274256ee5e
TheoGits <theokrist07@gmail.com>
2024-02-14 09:35:49 +0100

    7.1
    Javascript oppgave i KP

--- JavascriptVurdering/Oppgave2/script.js
+++ JavascriptVurdering/Oppgave2/script.js
@@ -6,16 +6,22 @@
 let Svar = document.getElementById('Svar')
 
 //denne gjør ord til array
-document.querySelector('#btn').addEventListener('click', function() {
+document.querySelector('#btn').addEventListener('click', function() {  
+    //document.querySelector('#btn') gjør slik button gjør funksjonen som blir lagt til av .addEventListener.
     const value = document.querySelector('#word').value
-
+    //dette setter value til det samme som blir skrivd inn
     const reversed = value.split('').reverse().join('')
+    //.split gjør hver karakter i det som blir skrivd inn til en array
+    //.reverse reverserer rekkefølgen på arrayen 
+    //.join legger sammen arrayen til et ord igjen
 
     if(value === reversed) {
+        //dette skjekker om ordet eller settningen er det samme reversert som det er til vanlig
         Svar.textContent = 'Ja, det er et palindrom!';
     } else {
         Svar.textContent = 'Nei, det er ikke et palindrom.';
     }
+    //svar.textcontent gjør slik vi får vite om det er et palindrom eller ei på nettsiden
 });
 
 
---
+++ U18-gambling/app.js
@@ -0,0 +1,7 @@
+document.getElementById('vippsButton').addEventListener('click', function() {
+    window.open('vipps-payment-link', '_blank');
+});
+
+if(valid){
+    
+}
